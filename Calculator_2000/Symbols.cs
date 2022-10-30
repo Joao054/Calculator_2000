d50967f934fc1eb9e110ea5d5cac5f831bd0b864
Joao054 <88591666+Joao054@users.noreply.github.com>
2022-10-29 23:13:28 -0300

    Add files via upload

---
+++ Calculator_2000/Symbols.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+public class Symbols
+{
+
+public static string symbolSUM = @"         
+           @
+           @
+           @
+           @
+  @@@@@@@@@@@@@@@@@@@
+           @
+           @
+           @
+           @
+           @
+
+  ";
+
+public static string symbolSUBS = 
+@"
+
+
+
+  *******************
+
+
+
+
+";
+
+public static string symbolMULT = 
+@"
+1111         1111
+ 1111       1111
+  1111     1111
+   1111   1111
+    1111 1111
+      11111
+    1111 1111
+   1111   1111
+  1111     1111
+ 1111       1111
+1111         1111
+
+  ";
+
+
+public static string symbolDIVIS = @"
+          @@
+          @@
+
+  *******************
+
+          @@
+          @@
+
+          ";
+
+public static string symbolPERC = @"
+          //
+         //
+  00    // 
+  00   //  
+      //  00
+     //   00
+    //
+   //
+  //
+  
+  ";
+
+
+public static string symbolSQUAREROOT = 
+@"
+                        1111111111111111
+                       11
+11111                 11
+    11               11
+     11             11     
+       11          11     
+        11        11    
+         11      11 
+          11    11
+           11  11
+            1111
+
+";
+
+public static string symbolPOW = 
+@"
+                 00   00
+                  00 00
+                   00
+                  00
+                 00
+ 111        111
+  111      111
+    111  111
+      1111
+    111  111
+   111    111
+ 111        111
+
+";
+
+}
